fd1a4d2b632e746d7346ab2d2c2d07cc3de81b28
Joao Vitor Romero da Cruz <jvromerocruz@gmail.com>
2022-04-12 11:30:23 -0300

    (feat): Create project, and implement HTTP Call for Currency;

---
+++ Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+var client = new HttpClient();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//TODO: Create specific models and controllers for Currency;
+app.MapGet("/currencies", async () => {
+    var stringTask = client.GetStringAsync("https://free.currconv.com/api/v7/convert?q=USD_EUR,EUR_USD&compact=ultra&apiKey=be2f246e873baa03563e");
+
+    var currencies  = await stringTask;
+    Console.Write(currencies);
+    return currencies;
+}).WithName("GetCurencies");
+;
+
+//TODO: Create specific models and controllers for Currency;
+app.MapGet("/countries", async () => {
+    
+    client.DefaultRequestHeaders.Accept.Clear();
+    var stringTask = client.GetStringAsync("https://free.currconv.com/api/v7/countries?apiKey=be2f246e873baa03563e");
+
+    var currencies  = await stringTask;
+    return currencies;
+}).WithName("GetCountries");
+;
+
+app.Run();
